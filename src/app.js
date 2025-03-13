e6ec4f30c62b715346c717d5a9bff48c7c53a2ca
Akash Sharma <sharma.54akash@gmail.com>
2025-03-14 01:08:02 +0530

    g

--- src/app.js
+++ src/app.js
@@ -7,14 +7,10 @@ const app = express()
 // In your Express app
 // At the very top of your Express app, before any other middleware
 app.use(cors({
-    origin: 'https://cru-dfrotnend.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
+    origin: 'https://cru-dfrotnend.vercel.app'
   }));
   
   // Also handle preflight OPTIONS requests
-  app.options('*', cors());
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
